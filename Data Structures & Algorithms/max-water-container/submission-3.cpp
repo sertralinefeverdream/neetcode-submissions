0c8753c4112751023e2460265314d37bd9a89a58
Adrian Graham <84514425+sertralinefeverdream@users.noreply.github.com>
2026-09-23 18:22:23 +0100

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        std::size_t i{0};
+        std::size_t j{heights.size() - 1};
+
+        int max{0};
+        while (j > i) {
+            const int h1{heights[i]};
+            const int h2{heights[j]};
+            const int area = std::min(h1, h2) * (j-i);
+            
+            max = std::max(max, area);
+
+            if (h1 <= h2) {
+                i++;
+            } else {
+                j--; 
+            }
+        }
+
+        return max;
+    }
+};
